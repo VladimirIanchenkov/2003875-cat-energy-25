a053c3a6c3616cd076d4c921364e1ea33ca5aa1a
VladimirIanchenkov <ianchenkov.v@gmail.com>
2022-05-05 14:49:45 +0700

    Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ const html = () => {
 const scripts = () => {
   return gulp.src('source/js/script.js')
   .pipe(terser())
+  .pipe(rename('script.min.js'))
   .pipe(gulp.dest('build/js'))
   .pipe(browser.stream());
 }
